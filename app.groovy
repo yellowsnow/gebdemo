2e98d2ba7450660add2d435ef489805fa61641ba
yellowsnow <contact@arkilog.ma>
2014-09-02 02:40:36 +0100

    Update app.groovy

--- app.groovy
+++ app.groovy
@@ -6,34 +6,13 @@
 import geb.Browser
  
 Browser.drive{
-   /* go "http://encodable.com/uploaddemo/"     
+    go "http://encodable.com/uploaddemo/"     
+    waitFor { title.endsWith("Upload a file") }
     def f = $('#theuploadform')
     f.uploadname1 = new File(System.getProperty('user.home'),'Desktop').absolutePath + '/GebDemo.groovy'
     f.subdir1 = '/'
     f.newsubdir1 = 'gebdemo'
     f.submit()
-    */
-    go "http://google.com/ncr"
- 
-    // make sure we actually got to the page
-    assert title == "Google"
- 
-    // enter wikipedia into the search field
-    $("input", name: "q").value("wikipedia")
- 
-    // wait for the change to results page to happen
-    // (google updates the page dynamically without a new request)
-    waitFor { title.endsWith("Google Search") }
- 
-    // is the first link to wikipedia?
-    def firstLink = $("li.g", 0).find("a.l")
-    assert firstLink.text() == "Wikipedia"
- 
-    // click the link
-    firstLink.click()
- 
-    // wait for Google's javascript to redirect to Wikipedia
-    waitFor { title == "Wikipedia" }
     
     println "DONE"
 }
